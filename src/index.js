791e5790533c96b60bc3985c70bc32a29eeeb76e
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-03-05 08:40:51 -0700

    update(config): Inject stylus into quasar conf to avoid ssr issues

--- src/index.js
+++ src/index.js
@@ -6,30 +6,6 @@
  */
 
 const extendWithFlashcard = function (api, conf) {
-  // make sure there is a framework section
-  if (!conf.framework) {
-    conf.framework = {}
-  }
-  
-  // make sure there is a directives array
-  if (!conf.framework.directives) {
-    conf.framework.directives = []
-  }
-
-  // for brevity
-  let directives = conf.framework.directives
-
-  // make sure directive CloseMenu is added
-  // if (!directives.includes('CloseMenu')) {
-  //   console.log(` App Extension (qmediaplayer) Info: 'Adding CloseMenu directive - consider adding this to your quasar.conf.js'`)
-  //   directives.push('CloseMenu')
-  // }
-  // make sure directive Ripple is added
-  // if (!directives.includes('Ripple')) {
-  //   console.log(` App Extension (qmediaplayer) Info: 'Adding Ripple directive - consider adding this to your quasar.conf.js'`)
-  //   directives.push('Ripple')
-  // }
-
   // make sure there is a boot array
   if (!conf.boot) {
     conf.boot = []
@@ -42,6 +18,15 @@ const extendWithFlashcard = function (api, conf) {
   if (!boot.includes('qflashcard')) {
     boot.push('qflashcard')
     console.log(` App Extension (qflashcard) Info: 'Adding qflashcard boot reference to your quasar.conf.js'`)
+  }
+
+  // for brevity
+  let css = conf.css
+
+  // make sure qmediaplayer css goes through webpack to avoid ssr issues
+  if (!css.includes('flashcard.styl')) {
+    css.push('@quasar/quasar-app-extension-qflashcard/component/flashcard.styl')
+    console.log(` App Extension (flashcard.styl) Info: 'Adding flashcard.styl css reference to your quasar.conf.js'`)
   }
 }
 
