080f9d761f953f5039101f2bd7a36c739a158654
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-03-06 08:16:38 -0700

    feat(QFlashCardSection): Now takes String or Array prop
    Allow combining transitions

--- component/QFlashcardSection.js
+++ component/QFlashcardSection.js
@@ -5,13 +5,44 @@ export default Vue.extend({
   name: 'QFlashcardSection',
 
   props: {
-    transition: String
+    transition: [String,Array]
+  },
+  computed: {
+    classes () {
+      if (this.transition === void 0) {
+        return null
+      }
+      let transition = this.transition
+      if (typeof this.transition === 'string') {
+        if (this.transition.includes(' ')) {
+          transition = this.transition.split(' ')
+        }
+        else {
+          return this.transitionName(this.transition)
+        }
+      }
+
+      if (Array.isArray(this.transition)) {
+        return this.transition.map(t => {
+          return this.transitionName(this.transition)
+        }).join(' ')
+      }
+    }
+  },
+
+  methods: {
+    transitionName (transition) {
+      if (transition.startsWith('fc-')) {
+        return transition
+      }
+      return 'fc-' + transition
+    }
   },
 
   render (h) {
     return h('div', {
       staticClass: 'q-flashcard--section',
-      class: this.transition !== void 0 ? 'fc-' + this.transition : null
+      class: this.classes
     }, slot(this, 'default'))
   }
 })
