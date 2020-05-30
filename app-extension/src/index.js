90f51f7f2bd41ba4d8eafdf5f535eef3c10c5402
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-05-30 09:10:47 -0600

    fix(app-ext): transpile quasar-ui-qflashcard code

--- app-extension/src/index.js
+++ app-extension/src/index.js
@@ -12,6 +12,7 @@ function extendConf (conf) {
 
   // make sure app extension files & ui package gets transpiled
   conf.build.transpileDependencies.push(/quasar-app-extension-qflashcard[\\/]src/)
+  conf.build.transpileDependencies.push(/quasar-ui-qflashcard[\\/]src/)
 
   // make sure the stylesheet goes through webpack to avoid SSR issues
   conf.css.push('~@quasar/quasar-ui-qflashcard/src/index.sass')
@@ -22,7 +23,7 @@ module.exports = function (api) {
   // hard dependencies, as in a minimum version of the "quasar"
   // package or a minimum version of "@quasar/app" CLI
   api.compatibleWith('quasar', '^1.5.0')
-  api.compatibleWith('@quasar/app', '^1.1.0')
+  api.compatibleWith('@quasar/app', '^1.3.0') // needed for describe to work properly
 
   // Uncomment the line below if you provide a JSON API for your component
   api.registerDescribeApi('QFlashcard', '~@quasar/quasar-ui-qflashcard/dist/api/QFlashcard.json')
